9f3e1a12e1691ce2d3b6dc48dcf8562a4f698c0b
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-18 17:43:35 -0600

    create the header component and place it at the top of my App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Header />
     </div>
   );
 }
