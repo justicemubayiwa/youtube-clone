cb6d7e8d71056910a02766ed687ce00139f297dc
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-18 17:24:15 -0600

    delete extra boilerplate css and js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6CNeGxpGtMohRf9CosFWvcgZYMo6nCgA",
+  authDomain: "clone-a8e1d.firebaseapp.com",
+  projectId: "clone-a8e1d",
+  storageBucket: "clone-a8e1d.appspot.com",
+  messagingSenderId: "562390429192",
+  appId: "1:562390429192:web:96f057160180b9e4ffabbd",
+  measurementId: "G-BVMD3JYQFM"
+};
