3eb8bfdfbdac9d088dd6d83af401ba5d212a66b4
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-18 21:04:15 -0600

    create 3 seperate child elements in the header div to divide left right and centre

--- src/Header.js
+++ src/Header.js
@@ -11,20 +11,28 @@ import Avatar from '@material-ui/core/Avatar';
 function Header() {
   return (
     <div className="header">
-      <MenuIcon />
-      <img
-        className="header_logo" 
-        alt="Youtube logo"
-        src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg"/>
-      <input type="text" />
+      <div className="header_left">
+        <MenuIcon />
+        <img
+          className="header_logo" 
+          alt="Youtube logo"
+          src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg"/>
+      </div>
+
+      <div className="header_input">
+        <input type="text" />
         <SearchIcon />
+      </div>
+      
+      <div className="header_right">
         <VideoCallIcon />
         <AppsIcon />
         <NotificationsIcon />
         <Avatar
           alt="Justice Mubayiwa"
-          src="https://yt3.ggpht.com/yti/APfAmoE1gOjOsyvo07UjkdCTyZ5Sr3kM2CVrN5oiOGJG6A=s88-c-k-c0x00ffffff-no-rj-mo"
-       />
+          src="https://yt3.ggpht.com/yti/APfAmoE1gOjOsyvo07UjkdCTyZ5Sr3kM2CVrN5oiOGJG6A=s88-c-k-c0x00ffffff-no-rj-mo"/>
+      </div>
+        
 
     </div>
   )
