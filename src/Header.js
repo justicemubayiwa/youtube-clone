7b0958232374813981a3e8dcfb0d824bbde7e3b9
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-18 18:19:38 -0600

    import SVG youtube logo to header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 function Header() {
   return (
     <div className="app">
       <h1>I am a header</h1>
+      <MenuIcon />
+      <img src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg" alt="youtube logo"/>
     </div>
   )
 }
