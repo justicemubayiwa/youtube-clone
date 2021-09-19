e20860e5364bd6a3ea6eb7b93bc5f7c9da2eb1a8
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-18 18:50:44 -0600

    import all icons from materialize for header. display header as a flex row

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
 
 
 function Header() {
   return (
-    <div className="app">
-      <h1>I am a header</h1>
+    <div className="header">
       <MenuIcon />
       <img
-      className="header_logo" 
-      src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg" alt="youtube logo"/>
+        className="header_logo" 
+        alt="Youtube logo"
+        src="https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg"/>
       <input type="text" />
-      <SearchIcon />
+        <SearchIcon />
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar
+          alt="Justice Mubayiwa"
+          src="https://yt3.ggpht.com/yti/APfAmoE1gOjOsyvo07UjkdCTyZ5Sr3kM2CVrN5oiOGJG6A=s88-c-k-c0x00ffffff-no-rj-mo"
+       />
+
     </div>
   )
 }
