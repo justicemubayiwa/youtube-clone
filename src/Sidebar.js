49d4f9a451858c028e0b9c59d6cd682c85e759db
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-19 18:38:46 -0600

    copy upper sidebar and import nems and icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,12 @@ import SideBarRow from './SideBarRow';
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OnDemandVideoIcon from '@material-ui/icons/Subscriptions';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 
 
 function Sidebar() {
@@ -12,6 +18,12 @@ function Sidebar() {
       <SideBarRow selected Icon={HomeIcon} title="Home" />
       <SideBarRow Icon={WhatshotIcon} title="Trending" />
       <SideBarRow Icon={SubscriptionIcon} title="Subscriptions" />
+      <hr/>
+      <SideBarRow Icon={VideoLibraryIcon} title="Library" />
+      <SideBarRow Icon={OnDemandVideoIcon} title="History" />
+      <SideBarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SideBarRow Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+      <SideBarRow Icon={ExpandMoreOutlinedIcon} title="Show More" />
       <hr/>
       
       
