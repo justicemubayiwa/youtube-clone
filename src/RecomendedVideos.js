ebedb01136cbef582d6067084f813e10ff8ec4db
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-19 22:15:18 -0600

    build recommended section without props

--- src/RecomendedVideos.js
+++ src/RecomendedVideos.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./RecomendedVideos.css"
 
+
 function RecomendedVideos() {
   return (
     <div className="recomendedVideos">
-      <h1>You should watch these</h1>
-      
+      <h2>Recomended</h2>
+      <div className-="recomendedVideos_videos">
+        <Video />
+        <Video /> 
+        <Video /> 
+        <Video /> 
+        <Video /> 
+        <Video /> 
+        <Video /> 
+        <Video />        
+      </div>
     </div>
   )
 }
