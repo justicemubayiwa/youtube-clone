ec9b96a085ef6a7565748e651bb01010748e6f09
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-19 23:55:34 -0600

    install realct router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import {BrowserRouter as Router, Switch,Route} from "react-router";
 
 import './App.css';
 
